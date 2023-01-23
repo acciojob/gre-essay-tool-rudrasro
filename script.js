9fa3bda3774c87dcd4dcfb2f8046d2e55555fa13
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-01-23 13:11:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your code here
 function updateWordCount() {
     var text = document.getElementById("evaluatedText").value;
-    var wordCount = text.split(" ").length;
-    document.getElementById("wordCount").innerHTML = wordCount;
+        var wordCount = text.trim().split(" ").length;
+        document.getElementById("wordCount").innerHTML = wordCount;
 }
